1b341da9c5af8e1dce50f645b0b055afc8528cdd
Jia Wen <chewjiawen98@gmail.com>
2018-01-15 00:31:25 +0800

    edit controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -64,12 +64,12 @@ recipeService.add($scope.recipe.name,
 
 
 
-.controller('recipeCtrl', ['$scope', '$stateParams','RecipeService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('recipeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,RecipeService) {
+function ($scope, $stateParams) {
 
-$scope.recipeArray = RecipeService.all();
+//$scope.recipeArray = recipeService.all();
 }])
 
 .controller('searchCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
