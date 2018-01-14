8d9e27bbdfe28c35a8ec51165d4e4139c094507a
Jia Wen <chewjiawen98@gmail.com>
2018-01-15 00:25:54 +0800

    recipe + search

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,8 +31,56 @@ function ($scope, $stateParams) {
 
 
 }])
-   
-.controller('recipeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('addrecipeCtrl', ['$scope', '$stateParams','recipeService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,recipeService) {
+
+  $scope.recipe = {
+      name: null,
+      image: null,
+      portion: null,
+      dificulty:null,
+      type:null,
+      date: new Date(),
+  };
+    
+$scope.add = function() {
+      
+recipeService.add($scope.recipe.name,
+
+                     $scope.recipe.image,
+
+                     $scope.recipe.portion,
+
+                     $scope.recipe.difficulty,
+
+                     $scope.recipe.type,
+                  
+                     $scope.recipe.date);
+  }
+}])
+
+
+
+.controller('recipeCtrl', ['$scope', '$stateParams','RecipeService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,RecipeService) {
+
+$scope.recipeArray = RecipeService.all();
+}])
+
+.controller('searchCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('categoryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
--- www/js/firebase-service.js
+++ www/js/firebase-service.js
@@ -1,6 +1,6 @@
 angular.module('firebaseService', ['firebase'])
 
-
+// ----------- HOME 
     .factory('HomeService', ['$firebaseArray',
 
         function ($firebaseArray) {
@@ -62,6 +62,76 @@ angular.module('firebaseService', ['firebase'])
                 delete: function (item) {
                     var item = HomeArray.$getRecord(item.$id);
                     HomeArray.$remove(item);
+                },
+            }
+        }])
+   
+// ----------- Recipe 
+    .factory('recipeService', ['$firebaseArray',
+
+        function ($firebaseArray) {
+
+            var config = {
+                apiKey: "AIzaSyDW5o-5259QI8DZ8I-IT0vKIFyB5P7ahRk",
+                authDomain: "tastychefdemo-20139.firebaseapp.com",
+                databaseURL: "https://tastychefdemo-20139.firebaseio.com",
+                projectId: "tastychefdemo-20139",
+                storageBucket: "tastychefdemo-20139.appspot.com",
+                messagingSenderId: "648974068968"
+            };
+
+            if (!firebase.apps.length) {
+                firebase.initializeApp(config);
+            }
+
+            var ref = firebase.database().ref().child("recipe").child;
+            
+            var recipeArray = $firebaseArray(ref);
+            
+            return {
+                add: function (rname, rimage, rportion, rdifficulty, rtype, rdate) {
+                    // Transform Date to string
+                    var recipeItem = {
+                        ref:rname,
+                        name:rname,
+                        image: rimage,
+                        portion: rportion,
+                        difficulty: rdifficulty,
+                        type:rtype, 
+                        date:rdate.toDateString()
+                    };
+                    recipeArray.$add(recipeItem);
+                    firebase
+                },
+
+                all: function () {
+                    return recipeArray.$loaded().then(function () {
+                        return recipeArray;
+                    });
+                },
+
+                get: function (item) {
+                    var item = recipeArray.$getRecord(item.$id);
+                    return item;
+                },
+
+                getPending: function () {
+                    var query = ref.orderByChild("status").equalTo("pending");
+                    var pendingArray = $firebaseArray(query);
+                    return pendingArray.$loaded().then(function () {
+                        return pendingArray;
+                    });
+                },
+
+                changeStatus: function (item, newStatus) {
+                    var item = recipeArray.$getRecord(item.$id);
+                    item.status = newStatus;
+                    recipeArray.$save(item);
+                },
+
+                delete: function (item) {
+                    var item = recipeArray.$getRecord(item.$id);
+                    recipeArray.$remove(item);
                 },
             }
         }])
